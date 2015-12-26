70313e22714c4af3050a7fee031f3ccbbbbee4b2
Corina <aio.corina@yahoo.com>
2015-12-26 11:06:15 +0200

    added minumum validations, fixed bug on shiw hide field based on checkbox

--- modal-window/color-render.js
+++ modal-window/color-render.js
@@ -1,22 +1,11 @@
 var pickColor = function() {
-	var colorInput = document.getElementsByClassName("modal-dialog__color-input")[0];
-	var colorSquare = document.getElementsByClassName("color-square")[0];
+	var colorInput = document.querySelector(".modal-dialog__color-input");
+	var colorSquare = document.querySelector(".color-square");
 	
-	if(colorInput && colorSquare) {		
+    if(colorInput && colorSquare) {		
 		colorSquare.style.fill = colorInput.value;
 		if(colorSquare.style.fill !== colorInput.value) {
-			console.log("ERROR");
-			colorSquare.style.fill="#ffffff";
+			colorSquare.style.fill="none";
 		}
 	}
 };
-
-document.querySelector('#vehicle-damaged').addEventListener('change', function(e){
-    var damageDetails = document.getElementsByClassName('modal-dialog__damage-details');
-    console.log(damageDetails.length);
-   for(var i=0; i< damageDetails.length; i++) {
-        var detail = damageDetails[i];
-        console.log(detail, i);
-        damageDetails[i].className += ' modal-dialog__damage-details--displayed';
-    }
-});
